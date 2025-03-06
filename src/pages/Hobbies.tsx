8d7cf5d0566deb3f8356553f10ae53e88f57c467
alvin <alvinzhu65@gmail.com>
2025-03-06 18:17:54 -0500

    adjust whitespace

--- src/pages/Hobbies.tsx
+++ src/pages/Hobbies.tsx
@@ -1,31 +1,58 @@
-
-import styled from 'styled-components'; 
-
+import styled from 'styled-components';
 
 const MainDiv = styled.div`
-margin-top : 0.75em;
-`; 
+  margin-top: 0.75em;
+  margin-bottom: 0; 
+`;
 
+const Title = styled.h3`
+font-size: 3em; 
+margin-left: 8em; 
+  margin-bottom: 1em; 
+`;
 
+const HobbiesList = styled.ul`
+  margin-top: 3em; /* Remove default margin */
+  padding: 0; /* Remove default padding */
+  list-style: none; /* Remove default bullet styling */
+`;
 
+const HobbiesListItem = styled.li`
+  margin-bottom: 0.5em; 
+  
+  &:last-child {
+    margin-bottom: 0em; 
+  }
+`;
 
-
-export default function Hobbies(){ 
-    return(
-        <>
-    <h3 id="main-title">My Hobbies</h3>
-            <MainDiv>
-                <div id="main-text3">
-                    <ul id = "hobbies">
-                        <li><strong>🏀 Basketball:</strong> I enjoy playing basketball and keeping active with friends.</li>
-                        <li><strong>💪 Lifting:</strong> Weightlifting is an important part of my fitness routine.</li>
-                        <li><strong>🎶 Music:</strong> I love listening to a wide variety of music genres, including R&B, hip hop, city pop, jazz, classical music, EDM.</li>
-                        <li><strong>🌆 Exploring Cities:</strong> I enjoy traveling and exploring new cities, getting to know their culture and architecture.</li>
-                        <li><strong>🍱 Trying New Foods:</strong> Food is a big part of my adventures; I love tasting different dishes wherever I go.</li>
-                        <li><strong>Meeting new people:</strong>I enjoy connecting and meeting new people, it is interesting to learn and hear their stories</li>
-                    </ul>
-                </div>
-            </MainDiv>
+export default function Hobbies() {
+  return (
+    <>
+      <Title>My Hobbies</Title>
+      <MainDiv>
+        <div id="main-text3">
+          <HobbiesList id="hobbies">
+            <HobbiesListItem>
+              <strong>🏀 Basketball:</strong> I enjoy playing basketball and keeping active with friends.
+            </HobbiesListItem>
+            <HobbiesListItem>
+              <strong>💪 Lifting:</strong> Weightlifting is an important part of my fitness routine.
+            </HobbiesListItem>
+            <HobbiesListItem>
+              <strong>🎶 Music:</strong> I love listening to a wide variety of music genres, including R&B, hip hop, city pop, jazz, classical music, EDM.
+            </HobbiesListItem>
+            <HobbiesListItem>
+              <strong>🌆 Exploring Cities:</strong> I enjoy traveling and exploring new cities, getting to know their culture and architecture.
+            </HobbiesListItem>
+            <HobbiesListItem>
+              <strong>🍱 Trying New Foods:</strong> Food is a big part of my adventures; I love tasting different dishes wherever I go.
+            </HobbiesListItem>
+            <HobbiesListItem>
+              <strong>Meeting new people:</strong> I enjoy connecting and meeting new people; it is interesting to learn and hear their stories.
+            </HobbiesListItem>
+          </HobbiesList>
+        </div>
+      </MainDiv>
     </>
-    )
+  );
 }
